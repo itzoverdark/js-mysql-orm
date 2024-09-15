ab532e37c751dd00ff13d396412068a8db9183dc
ishaq <i.ifkirne@yahoo.com>
2024-09-15 13:45:12 +0100

    test for database connection

---
+++ tests/database.test.js
@@ -0,0 +1,26 @@
+const Database = require("../database/Database.js");
+
+async function runTests() {
+    const db = new Database(
+        "127.0.0.1", //host
+        "root", //user
+        "ishaq", //password
+        "customorm", //database
+        3306, //port
+    );
+
+    console.log("Test 1 : DATABASE CONNECTION");
+
+    try {
+        const connection = await db.connect();
+        if (connection) {
+            console.log('PASS: Database connected successfully');
+        } else {
+            console.log('FAIL: Database connection failed');
+        }
+    } catch (err) {
+        console.log("DATABASE CONNECTION ERROR : ", err);
+    }
+}
+
+runTests();
