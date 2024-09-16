c9c580493d52b1be772cc900580e41756df61515
ishaq <i.ifkirne@yahoo.com>
2024-09-16 14:38:02 +0100

    move each query code to its file in utils

--- database/Database.js
+++ database/Database.js
@@ -1,4 +1,6 @@
 const mysql = require("mysql2/promise");
+const selectQueryBuilder = require("./utils/selectQueryBuilder");
+const InsertQueryBuilder = require("./utils/insertQueryBuilder");
 
 class Database {
     constructor(host, user, password, database, port) {
@@ -74,34 +76,12 @@ class Database {
         }
     };
 
-    async insert(tableName, data) {
+    insert(tableName) {
         if (!this.connection) {
             throw new Error("Database connection not established.");
         }
-
-        // Ensure data is an array
-        if (!Array.isArray(data)) {
-            data = [data];
-        }
-
-        if (data.length === 0) {
-            throw new Error("Data array should not be empty.");
-        }
-
-        const columns = Object.keys(data[0]).join(", ");
-        const values = data.map(record => Object.values(record));
-        const placeholders = values[0].map(() => '?').join(", ");
-        const sql = `INSERT INTO ${tableName} (${columns}) VALUES (${placeholders})`;
-
-        try {
-            for (const record of data) {
-                await this.connection.query(sql, Object.values(record));
-                console.log(`Record inserted into ${tableName} successfully.`);
-            }
-        } catch (err) {
-            console.error(`ERROR INSERTING RECORD INTO ${tableName}:`, err);
-        }
-    };
+        return new InsertQueryBuilder(this, tableName);
+    }
 
     select(tableName) {
         if (!this.connection) {
@@ -109,71 +89,9 @@ class Database {
         }
 
         // Return an instance of QueryBuilder
-        return new QueryBuilder(this, tableName);
-    }
-    
-    
-    
-}
-
-
-class QueryBuilder {
-    constructor(db, tableName) {
-        this.db = db;
-        this.tableName = tableName;
-        this.columnsList = "*"; // Default to all columns
-        this.whereClause = "";
-        this.additionalConditions = [];
-        this.parameters = [];
-    }
-
-    columns(...columns) {
-        this.columnsList = columns.length > 0 ? columns.join(", ") : "*";
-        return this; // Return `this` to allow chaining
-    }
-
-    where(condition, parameter) {
-        this.whereClause = ` WHERE ${condition}`;
-        if (parameter !== undefined) {
-            this.parameters.push(parameter);
-        }
-        return this; // Return `this` to allow chaining
-    }
-
-    and(condition, parameter) {
-        if (this.whereClause === "") {
-            this.whereClause = ` WHERE ${condition}`;
-        } else {
-            this.additionalConditions.push(`AND ${condition}`);
-        }
-        if (parameter !== undefined) {
-            this.parameters.push(parameter);
-        }
-        return this; // Return `this` to allow chaining
-    }
-
-    or(condition, parameter) {
-        if (this.whereClause === "") {
-            this.whereClause = ` WHERE ${condition}`;
-        } else {
-            this.additionalConditions.push(`OR ${condition}`);
-        }
-        if (parameter !== undefined) {
-            this.parameters.push(parameter);
-        }
-        return this; // Return `this` to allow chaining
-    }
-
-    async execute() {
-        const fullQuery = `SELECT ${this.columnsList} FROM ${this.tableName}${this.whereClause} ${this.additionalConditions.join(" ")}`;
-        try {
-            const [rows] = await this.db.connection.query(fullQuery, this.parameters);
-            console.log(rows);
-            return rows;
-        } catch (err) {
-            console.error("Error executing query:", err);
-            throw err;
-        }
+        return new selectQueryBuilder(this, tableName);
     }
 }
+
+
 module.exports = Database;
