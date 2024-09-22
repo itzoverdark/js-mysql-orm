e4c6cef2d4fc9985bb678f35eefe78b45a540407
ishaq <i.ifkirne@yahoo.com>
2024-09-22 20:09:09 +0100

    add detailed documentation for insert  query builder

--- database/utils/dataOperations/insertQueryBuilder.js
+++ database/utils/dataOperations/insertQueryBuilder.js
@@ -1,4 +1,11 @@
 class InsertQueryBuilder {
+    /**
+     * InsertQueryBuilder class facilitates the construction and execution of
+     * INSERT SQL queries with specified records.
+     *
+     * @param {object} db - The database instance to interact with.
+     * @param {string} tableName - The name of the table where records will be inserted.
+     */
     constructor(db, tableName) {
         this.db = db;
         this.tableName = tableName;
@@ -6,7 +13,23 @@ class InsertQueryBuilder {
         this.values = [];
     }
 
-    // Add records for insertion (either a single object or an array of objects)
+    /**
+     * Specifies the records to be inserted into the table. Accepts either
+     * a single object or an array of objects.
+     *
+     * @param {object|Array<object>} data - The record(s) to be inserted, where each
+     * object represents a row with key-value pairs as column names and their values.
+     * @returns {InsertQueryBuilder} Returns the instance of the query builder for chaining.
+     * @throws {Error} If the data array is empty.
+     * 
+     * @example
+     * const query = new InsertQueryBuilder(db, "users");
+     * query.records({name: "John", age: 25}).execute();
+     * 
+     * @example
+     * const query = new InsertQueryBuilder(db, "users");
+     * query.records([{name: "John", age: 25}, {name: "Jane", age: 30}]).execute();
+     */
     records(data) {
         if (!Array.isArray(data)) {
             data = [data];
@@ -21,6 +44,17 @@ class InsertQueryBuilder {
         return this;
     }
 
+    /**
+     * Executes the INSERT query to insert the specified records into the table.
+     * Each record will be inserted individually.
+     * 
+     * @returns {Promise<void>} Resolves if the query executes successfully.
+     * @throws {Error} If the database connection is not established or no records are provided for insertion.
+     * 
+     * @example
+     * const query = new InsertQueryBuilder(db, "users");
+     * query.records([{name: "John", age: 25}, {name: "Jane", age: 30}]).execute();
+     */
     async execute() {
         if (!this.db.connection) {
             throw new Error("Database connection not established.");
